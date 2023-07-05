f18d0455119ae6e6d189dbb02cbe7a36637dfd40
Federico Dacal <federicodacal@gmail.com>
2023-07-04 21:23:59 -0300

    mis turnos empezando filtros

--- clinica_online/src/app/pages/mis-turnos/mis-turnos.component.ts
+++ clinica_online/src/app/pages/mis-turnos/mis-turnos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { DatabaseService } from 'src/app/services/database.service';
 import { ToastService } from 'src/app/services/toast.service';
@@ -8,13 +8,24 @@ import { ToastService } from 'src/app/services/toast.service';
   templateUrl: './mis-turnos.component.html',
   styleUrls: ['./mis-turnos.component.css']
 })
-export class MisTurnosComponent {
+export class MisTurnosComponent implements OnInit, OnDestroy {
 
   turnos:any[] = [];
   user:any = {};
 
   esPaciente!:boolean;
   esEspecialista!:boolean;
+
+  arrayTurnos:any[] = []
+  arrayEspecialistas:any[] = [];
+  arrayEspecialidades:any[] = [];
+  arrayPacientes:any[] = [];
+
+  pacienteSeleccionado:any = {};
+  especialidadSeleccionada:any = {};
+  especialistaSeleccionado:any = {};
+
+  mostrarFiltro:string='';
 
   loading:boolean = false;
  
@@ -30,6 +41,17 @@ export class MisTurnosComponent {
           this.esEspecialista = false;
           this.db.getTurnosByPaciente(this.user.uid).subscribe((res:any) => {
             this.turnos = res;
+            console.log('turnos', this.turnos);
+
+            this.turnos.forEach((t:any) => {
+              if(!this.arrayEspecialidades.includes(t.especialidad)) {
+                this.arrayEspecialidades.push(t.especialidad);
+              }
+
+              if(!this.arrayEspecialistas.some(esp => esp.uid === t.uidEspecialista)) {
+                this.arrayEspecialistas.push({uid:t.uidEspecialista, nombre:t.nombreEspecialista})
+              }
+            });
           });
         }
         else if(this.user.perfil == 'especialista') {
@@ -37,10 +59,48 @@ export class MisTurnosComponent {
           this.esPaciente = false;
           this.db.getTurnosByEspecialista(this.user.uid).subscribe((res:any) => {
             this.turnos = res;
+            console.log('turnos', this.turnos);
+
+            this.turnos.forEach((t:any) => {
+              if(!this.arrayEspecialidades.includes(t.especialidad)) {
+                this.arrayEspecialidades.push(t.especialidad);
+              }
+
+              if(!this.arrayPacientes.some(pac => pac.uid === t.uidPaciente)) {
+                this.arrayPacientes.push({uid:t.uidPaciente, nombre:t.nombrePaciente})
+              }
+            });
           });
         }
 
     });
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  onClickFiltro(queFiltro:string) {
+    if(queFiltro == 'especialidad') {
+      this.mostrarFiltro = 'especialidad';
+    }
+    else if(queFiltro == 'especialista') {
+      this.mostrarFiltro = 'especialista';
+    }
+    else if(queFiltro == 'paciente') {
+      this.mostrarFiltro = 'paciente';
+    }
+  }
+
+  onClickFiltroEspecialidad(especialidad:string) {
+
+  }
+
+  onClickFiltroEspecialista(uid:string) {
+
+  }
+
+  onClickFiltroPaciente(uid:string) {
 
   }
 
