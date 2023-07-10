23409d50d979af89cc7ad1f6e9537ea44c1b95bb
Federico Dacal <federicodacal@gmail.com>
2023-07-10 19:16:24 -0300

    descarga de excel de usuarios y de turnos pacientes en selec usuarios

--- clinica_online/src/app/pages/seleccion-usuarios/seleccion-usuarios.component.ts
+++ clinica_online/src/app/pages/seleccion-usuarios/seleccion-usuarios.component.ts
@@ -3,6 +3,13 @@ import { Subscription } from 'rxjs';
 import { DatabaseService } from 'src/app/services/database.service';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import * as FileSaver from 'file-saver';
+import { saveAs } from 'file-saver';
+import * as XLSX from 'xlsx';
+import { ToastService } from 'src/app/services/toast.service';
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+
 
 @Component({
   selector: 'app-seleccion-usuarios',
@@ -30,7 +37,7 @@ export class SeleccionUsuariosComponent implements OnInit, OnDestroy {
   subscription!:Subscription;
   subscriptionPaciente!:Subscription;
 
-  constructor(private db:DatabaseService) { }
+  constructor(private db:DatabaseService, private toast:ToastService) { }
 
   ngOnInit(): void {
 
@@ -116,8 +123,40 @@ export class SeleccionUsuariosComponent implements OnInit, OnDestroy {
       });
   }
 
-  crearExcel() {
-    
+  exportAsExcelFile(json: any[], excelFileName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { data: worksheet },
+      SheetNames: ['data'],
+    };
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(
+      data,
+      fileName + '_excel' + EXCEL_EXTENSION
+    );
+  }
+
+  crearExcelUsuarios() {
+    this.exportAsExcelFile(this.users, 'usuarios');
+  }
+
+  crearExcelUsuarioSeleccionado(user:any) {
+    if(user.perfil != 'paciente') {
+      this.toast.showWarning('Operación no permitida', 'La descarga del historial de turnos puede realizar en usuarios de perfil paciente.')
+    }
+    else {
+      this.db.getTurnosByPaciente(user.uid).subscribe((res:any) => {
+        this.exportAsExcelFile(res, `turnos_${user.lastName}`);
+      });
+    }
   }
 
 }
