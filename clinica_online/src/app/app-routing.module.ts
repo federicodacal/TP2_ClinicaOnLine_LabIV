53b38b57032ffed0ba1add9d9326e66bf0da5c78
Federico Dacal <federicodacal@gmail.com>
2023-07-12 15:40:27 -0300

    animations

--- clinica_online/src/app/app-routing.module.ts
+++ clinica_online/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SeleccionUsuariosComponent } from './pages/seleccion-usuarios/seleccion-usuarios.component';
@@ -11,39 +10,40 @@ import { TurnosComponent } from './pages/turnos/turnos.component';
 import { AdminGuard } from './guards/admin.guard';
 import { PacientesComponent } from './pages/pacientes/pacientes.component';
 import { InformesComponent } from './pages/informes/informes.component';
+import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
 
 const routes: Routes = [
   { 
-    path: '', component:BienvenidaComponent 
+    path: '', component:BienvenidaComponent, data: {animation: 'Home'}
   }, 
   { 
-    path: 'registro', component:RegistroComponent 
+    path: 'registro', component:RegistroComponent,  
   },
   { 
-    path: 'login', component:LoginComponent 
+    path: 'login', component:LoginComponent, data: {animation: 'Login'} 
   },
   { 
-    path: 'seleccion-usuarios', component:SeleccionUsuariosComponent, 
+    path: 'seleccion-usuarios', component:SeleccionUsuariosComponent,  
     canActivate: [AdminGuard] 
   },
   { 
-    path: 'mis-turnos', component:MisTurnosComponent 
+    path: 'mis-turnos', component:MisTurnosComponent,   
   },
   { 
-    path: 'mi-perfil', component:MiPerfilComponent 
+    path: 'mi-perfil', component:MiPerfilComponent, data: {animation: 'Usuario'}
   },
   { 
-    path: 'solicitar-turno', component:SolicitarTurnoComponent 
+    path: 'solicitar-turno', component:SolicitarTurnoComponent,  
   },
   { 
     path: 'turnos', component:TurnosComponent,
     canActivate: [AdminGuard] 
   },
   {
-    path: 'pacientes', component:PacientesComponent
+    path: 'pacientes', component:PacientesComponent,  
   },
   {
-    path: 'informes', component:InformesComponent
+    path: 'informes', component:InformesComponent,  
   }
 ];
 
